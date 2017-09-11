846ad5f6c9a3adbdd66c7b4a38003e8320f13618
Techcadia <info@techcadia.com>
2017-09-11 14:02:11 -0700

    [Add] Buildsteps to Project

---
+++ teamcity/build_configuration_replace_all_steps.go
@@ -0,0 +1,24 @@
+package teamcity
+
+import (
+	"errors"
+	"fmt"
+	"github.com/umweltdk/teamcity/types"
+)
+
+func (c *Client) ReplaceAllBuildConfigurationSteps(buildConfID string, steps *types.BuildSteps) error {
+	path := fmt.Sprintf("/httpAuth/app/rest/buildTypes/id:%s/steps", buildConfID)
+	var buildstepsReturn *types.BuildSteps
+
+	err := c.doRetryRequest("PUT", path, steps, &buildstepsReturn)
+	if err != nil {
+		return err
+	}
+
+	if buildstepsReturn == nil {
+		return errors.New("build configuration steps not updated")
+	}
+	*steps = *buildstepsReturn
+
+	return nil
+}
