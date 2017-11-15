7d1e313200315e2401de455b2452839278a435b6
Techcadia <info@techcadia.com>
2017-11-15 15:58:50 -0800

    [Add] Support for Single Parameter Replace

---
+++ teamcity/build_configuration_replace_parameter_value.go
@@ -0,0 +1,17 @@
+package teamcity
+
+import (
+	"bytes"
+	"fmt"
+)
+
+func (c *Client) ReplaceBuildConfigurationParameterValue(buildConfID, name string, value string) error {
+	path := fmt.Sprintf("/httpAuth/app/rest/buildTypes/id:%s/parameters/%s", buildConfID, name)
+
+	body := bytes.NewBuffer([]byte(value))
+	_, err := c.doNotJSONRequest("PUT", path, "text/plain", "text/plain", body)
+	if err != nil {
+		return err
+	}
+	return nil
+}
